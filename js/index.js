7c237c4c3ba83ff1658879f8e0c38d07b4e57120
«Vasiliy <v_bibas@mail.ru>
2024-01-09 22:30:43 +0300

    added files

---
+++ js/index.js
@@ -0,0 +1,48 @@
+
+const App = {
+	data: () => ({
+		newArticle: [
+			{
+				id: 1,
+				imgArticle: "img/article_photo_1.jpg",
+				titleArticle: "Let’s Get Solution For Building Construction Work",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 2,
+				imgArticle: "img/article_photo_2.jpg",
+				titleArticle: "Low Cost Latest Invented Interior Designing Ideas.",
+				dateArticle: "22 December,2022"
+			},
+			{
+				id: 3,
+				imgArticle: "img/article_photo_3.jpg",
+				titleArticle: "Best For Any Office & Business Interior Solution",
+				dateArticle: "25 December,2022"
+			},
+			{
+				id: 4,
+				imgArticle: "img/article_photo_4.jpg",
+				titleArticle: "Let’s Get Solution For Building Construction Work",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 5,
+				imgArticle: "img/article_photo_5.jpg",
+				titleArticle: "Low Cost Latest Invented Interior Designing Ideas.",
+				dateArticle: "26 December,2022"
+			},
+			{
+				id: 6,
+				imgArticle: "img/article_photo_6.jpg",
+				titleArticle: "Best For Any Office & Business Interior Solution",
+				dateArticle: "26 December,2022"
+			},
+		]
+	}),
+	methods: {
+		
+	},
+
+};
+Vue.createApp(App).mount("#app");
